10afc5597235f4e0dc5c40601bac60d6506c37b2
Geoffrey <geoffrey.couten@gmail.com>
2020-11-17 15:08:35 +0100

    faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }
