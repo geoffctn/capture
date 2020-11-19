8daf13b204f0f0448b575d5067a45582bfd3c34d
Geoffrey <geoffrey.couten@gmail.com>
2020-11-19 07:50:40 +0100

    added line nav anim

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 
 const Nav = () => {
+  const { pathname } = useLocation()
   return (
     <StyledNav>
       <h1>
@@ -13,12 +16,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">1. About Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname === '/' ? '50%' : '0%' }}
+          />
         </li>
         <li>
           <Link to="/work">2. Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname === '/work' ? '50%' : '0%' }}
+          />
         </li>
         <li>
           <Link to="/contact">3. Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname === '/contact' ? '50%' : '0%' }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -68,6 +86,18 @@ const StyledNav = styled.nav`
         padding: 0;
       }
     }
+  }
+`
+
+const Line = styled(motion.div)`
+  position: absolute;
+  left: 60%;
+  bottom: -80%;
+  width: 0;
+  height: 0.3rem;
+  background: #23d997;
+  @media (max-width: 1300px) {
+    left: 0;
   }
 `
 
